ce9893d2c1c667acb8c119189d67d5456548bc2a
Nokuthulan Ngcongo <nokuthulangcongo11@gmail.com>
2025-02-27 11:24:25 +1200

    Created login page, But ni still have troubles with configuration

---
+++ src/firebaseconfig.js
@@ -0,0 +1,9 @@
+
+
+const firebaseConfig = {
+apiKey: "AIzaSyDSzef8X_3B1k2HW5DAICJPCsCiDmYFgL8", authDomain: "breast-cancer-awareness-7539d.firebaseapp.com",
+projectId:"breast-cancer-awareness-7539d", storageBucket: "breast-cancer-awareness-7539d.appspot.com",
+messagingSenderId: "666071902319", appId: "1:666071902319:web:d096406f6bab9c9057859a"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
--- src/script.js
+++ src/script.js
@@ -1,34 +1,55 @@
-function login(){
-const email= document.getElementById("loginEmail").value;
-const password = document.getElementById("loginPassword").value;
 
-auth.signInWithEmailAndPassword(email, password).then(() => {
-window.location.href = "main.html";
-}
-)
-.catch(error => alert(error.message));
-}
 
-function signUp(){
-const email= document.getElementById("signUp").value;
-const password = document.getElementById("signupPassword").value;
+document.addEventListener("DOMContentLoaded", function (){
+firebase.initializeApp(firebaseConfig);
 
-auth.createUserWithEmailAndPassword(email, password).then(userCredential => {
-userCredential.user.sendEmailVerification();
-alert("Verification email sent: Please check your inbox.");
 
+
+
+
+const loginBtn = document.getElementById("loginBtn");
+if(loginBtn){
+loginBtn.addEventListener("click", function(){
+let email =document.getElementById("email").value;
+let password =document.getElementById("password").value;
+
+firebase.auth().signInWithEmailAndPassword(email, password).then(userCredential) =>{
+alert("Login successful!");
+
+window.location.href = ",main.html";
 })
+        .catch((error) => {alert("Error: " + error.message);
+});
+});
+}
+const signUpBtn = document.getElementById("signUpBtn");
+if(signUpBtn){
+signUpBtn.addEventListener("click", function(){
+let email =document.getElementById("email").value;
+let password =document.getElementById("password").value;
 
-.catch(error => alert(error.message));
+firebase.auth().signInWithEmailAndPassword(email, password).then(userCredential) =>{
+alert("Sign-up successful! Check your email now.");
+
+userCredential.user.sendEmailVerification();
+})
+        .catch((error) => {alert("Error: " + error.message);
+});
+});
+}
+const resetPasswordBtn = document.getElementById("resetPasswordBtn");
+if(resetPasswordBtn){
+resetPasswordBtn.addEventListener("click", function(){
+let email =document.getElementById("email").value;
+
+
+firebase.auth().sendPasswordResetEmail(email).then(() => {
+alert("password reset email sent! Check your email now.");
+
+userCredential.user.sendEmailVerification();
+})
+        .catch((error) => {alert("Error: " + error.message);
+});
+});
 }
 
-function forgotPassword(){
-const email = document.getElementById("loginEmail").value;
-
-auth.sendPasswordResetEmail(email)
-.then(() => alert("Password reset email sent.")).catch(error => alert(error.message));
-}
-
-function logout(){
-auth.signOut().then(() => window.location.href ="index.html").catch(error.message));
-}
